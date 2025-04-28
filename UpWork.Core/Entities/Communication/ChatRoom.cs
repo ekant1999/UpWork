c63f5a24e3992cd17d513582d4d964d9ee2fdd4d
Ekant Kapgate <ekant.kapgate@optym.com>
2025-04-28 16:47:28 +0530

    added the entities for communication

---
+++ UpWork.Core/Entities/Communication/ChatRoom.cs
@@ -0,0 +1,17 @@
+﻿using UpWork.UpWork.Core.Entities.Communication;
+
+namespace UpWork.Core.Entities.Communication
+{
+    public class ChatRoom
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int? RelatedJobId { get; set; }
+        public int? RelatedContractId { get; set; }
+
+        // Navigation properties
+        public virtual ICollection<Message> Messages { get; set; }
+        public virtual ICollection<ChatRoomUser> Users { get; set; }
+    }
+}
---
+++ UpWork.Core/Entities/Communication/Message.cs
@@ -0,0 +1,20 @@
+﻿using UpWork.Core.Entities.Communication;
+using UpWork.Core.Entities.Identity;
+
+namespace UpWork.UpWork.Core.Entities.Communication
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int ChatRoomId { get; set; }
+        public string SenderId { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; }
+        public bool IsRead { get; set; }
+
+        // Navigation properties
+        public virtual ChatRoom ChatRoom { get; set; }
+        public virtual ApplicationUser Sender { get; set; }
+        public virtual ICollection<FileAttachment> Attachments { get; set; }
+    }
+}
