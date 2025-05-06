7907420433880eb7d8161d1b03314dfe3e7f4155
Ekant Kapgate <ekant.kapgate@optym.com>
2025-05-06 23:53:29 +0530

    implemented initial draft of Create user.

--- Program.cs
+++ Program.cs
@@ -23,7 +23,6 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
